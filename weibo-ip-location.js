04d2e86775f83e0a22bbdb2ccca9ebcc99587e32
NiaoBlush <303138518@qq.com>
2022-03-22 23:53:07 +0800

    获取地址与标记

--- weibo-ip-location.js
+++ weibo-ip-location.js
@@ -5,7 +5,7 @@
 // @license         MIT
 // @grant           none
 // @include         https://weibo.com/*
-// @require         https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// @require         https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js
 
 
 (function () {
@@ -13,24 +13,56 @@
 
     const getLocation = (uid) => {
         return new Promise((resolve, reject) => {
-            $.get(`https://weibo.com/ajax/profile/info?uid=${uid}`, function (res) {
-                if (res.data && res.data.user && res.data.user.location) {
-                    resolve(res.data.user.location)
-                } else {
-                    resolve("")
-                }
-            })
+            setTimeout(function () {
+                $.get(`https://weibo.com/ajax/profile/info?uid=${uid}`, function (res) {
+                    if (res.data && res.data.user && res.data.user.location) {
+                        resolve(res.data.user.location)
+                    } else {
+                        resolve("")
+                    }
+                })
+            }, 500)
         })
+    }
+
+    const mark = (jObj, location) => {
+        // jObj.css("border", "solid 2px red");
+        jObj.append(`<span style="background-color: red;color: #FFF;margin-left: 5px;font-weight: bold;border-radius: 8px;padding: 2px 5px;">${location}</span>`)
     }
 
     console.log("hello");
     console.log("$.fn.jquery", $.fn.jquery);
 
-    getLocation(6253476127)
-        .then((l) => {
-            console.log("l", l);
-        })
+    const locationMap = {}
 
+    //先暂时这样 跳过载入完成的判断
+    setTimeout(function () {
+        const list = $("a.ALink_default_2ibt1:not([aria-label])")
+        list.each(function () {
+            const href = $(this).attr("href");
+            const array = /\/u\/(\d+)/.exec(href)
+            if (array != null) {
+                const uid = array[1];
+                if (!locationMap[uid]) {
+                    locationMap[uid] = "pending"
+                    getLocation(uid)
+                        .then(l => {
+                            locationMap[uid] = l;
+                            console.log("l", l);
+                            if (l.startsWith("海外")) {
+                                mark($(this), l)
+                            }
+                        })
+                } else {
+
+                }
+                // console.log("uid", uid);
+            }
+            // console.log("href", href);
+        })
+        // const list=$("a")
+        // console.log("list a", list);
+    }, 1500)
 
 
 })();
